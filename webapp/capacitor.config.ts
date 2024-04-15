b2cd8a2628b464364641d1245f8048a74e7b2dff
gb88 <gb8253@gmail.com>
2024-04-15 12:47:03 +0200

    webapp source

---
+++ webapp/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Esp32BleOta',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
